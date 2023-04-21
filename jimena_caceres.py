18d23562f91b1506f92e1e05d5f703c1552c3df1
Yenaro Joel Noa Camino <97652778+Diegoo11@users.noreply.github.com>
2023-04-21 14:10:25 +0000

    fixed erros comun

--- jimena_caceres.py
+++ jimena_caceres.py
@@ -22,9 +22,28 @@ def view_table():
 
 
 def test_positions(num):
-    if num <= 2 and num >= 0:
+    if 0 <= num <= 2:
         return True
     return False
+
+
+def ask_positions():
+    row = int(input("Escoge una fila entre 0 y 2 = "))
+    while not test_positions(row):
+        row = int(input("Escoge una fila entre 0 y 2 = "))
+
+    column = int(input("Escoge una columna entre 0 y 2 = "))
+    while not test_positions(column):
+        column = int(input("Escoge una columna entre 0 y 2 = "))
+
+    return [row, column]
+
+
+def test_rules(coord_row, coord_column):
+    content = square_game[coord_row][coord_column]
+    if not content == " ":
+        return False
+    return True
 
 
 def game():
@@ -36,19 +55,27 @@ def game():
         view_table()
         if turno == 1:
             print("le toca al jugador X")
-            row = input("Escoge una fila entre 0 y 2 = ")
-            column = input("Escoge una columna entre 0 y 2 = ")
-            if not (test_positions(row) and test_positions(column)):
-                print("pwaodapwopo")
-            square_game[int(row)][int(column)] = "X"
+
+            positions = ask_positions()
+
+            while not test_rules(positions[0], positions[1]):
+                positions = ask_positions()
+
+            square_game[positions[0]][positions[1]] = "X"
             turno = 0
+
         else:
             print("le toca al jugador O")
-            row = input("Escoge una fila entre 0 y 2 = ")
-            column = input("Escoge una columna entre 0 y 2 = ")
-            # square_game[row][column] = "O"
-            square_game[int(row)][int(column)] = "O"
+
+            positions = ask_positions()
+
+            while not test_rules(positions[0], positions[1]):
+                positions = ask_positions()
+
+            square_game[positions[0]][positions[1]] = "O"
             turno = 1
+
+        print("------------------------------------")
 
 
 game()
